4f171f34f28532a0f600d2283ddd8a4282c90522
lordyhas <dev.haspro@gmail.com>
2023-10-13 19:50:34 +0200

    auth bloc updated : user | v0.2.0+3

--- lib/data/app_bloc/auth_repository/user.dart
+++ lib/data/app_bloc/auth_repository/user.dart
@@ -81,6 +81,14 @@ class User extends Equatable {
   /// Empty user which represents an unauthenticated user.
   static const empty = User(email: '', id: '', name: null, photoMail: null);
 
+
+
+  /// Convenience getter to determine whether the current user is empty.
+  bool get isEmpty => this == User.empty;
+
+  /// Convenience getter to determine whether the current user is not empty.
+  bool get isNotEmpty => this != User.empty;
+
   /// copyWith method, will help to get [UserData] saved in FireCloud
   User copyWith({
     String? id,
