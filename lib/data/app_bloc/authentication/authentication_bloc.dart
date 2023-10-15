17999f23166e46e0e888869623773888b9a11bef
lordyhas <dev.haspro@gmail.com>
2023-10-15 20:03:59 +0200

    auth bloc | v0.2.0+6

--- lib/data/app_bloc/authentication/authentication_bloc.dart
+++ lib/data/app_bloc/authentication/authentication_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pedantic/pedantic.dart';
 
 import '../../authentication_repository.dart';
