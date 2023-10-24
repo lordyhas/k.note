9d5cde0a1259e7a15c3c46f2ba52ea0678f107a2
lordyhas <dev.haspro@gmail.com>
2023-10-24 12:25:39 +0200

    auth repo updated | version 0.2.1+1

--- lib/data/app_bloc/auth_repository/repository.dart
+++ lib/data/app_bloc/auth_repository/repository.dart
@@ -100,7 +100,57 @@ class LogInAnonymouslyFailure implements Exception {}
 class LogInWithFacebookFailure implements Exception {}
 
 /// Thrown during the sign in with google process if a failure occurs.
-class LogInWithGoogleFailure implements Exception {}
+
+class LogInWithGoogleFailure implements Exception {
+  /// {@macro log_in_with_google_failure}
+  const LogInWithGoogleFailure([
+    this.message = 'An unknown exception occurred.',
+  ]);
+
+  /// Create an authentication message
+  /// from a firebase authentication exception code.
+  factory LogInWithGoogleFailure.fromCode(String code) {
+    switch (code) {
+      case 'account-exists-with-different-credential':
+        return const LogInWithGoogleFailure(
+          'Account exists with different credentials.',
+        );
+      case 'invalid-credential':
+        return const LogInWithGoogleFailure(
+          'The credential received is malformed or has expired.',
+        );
+      case 'operation-not-allowed':
+        return const LogInWithGoogleFailure(
+          'Operation is not allowed.  Please contact support.',
+        );
+      case 'user-disabled':
+        return const LogInWithGoogleFailure(
+          'This user has been disabled. Please contact support for help.',
+        );
+      case 'user-not-found':
+        return const LogInWithGoogleFailure(
+          'Email is not found, please create an account.',
+        );
+      case 'wrong-password':
+        return const LogInWithGoogleFailure(
+          'Incorrect password, please try again.',
+        );
+      case 'invalid-verification-code':
+        return const LogInWithGoogleFailure(
+          'The credential verification code received is invalid.',
+        );
+      case 'invalid-verification-id':
+        return const LogInWithGoogleFailure(
+          'The credential verification ID received is invalid.',
+        );
+      default:
+        return const LogInWithGoogleFailure();
+    }
+  }
+
+  /// The associated error message.
+  final String message;
+}
 
 /// Thrown during the logout process if a failure occurs.
 class LogOutFailure implements Exception {}
@@ -135,17 +185,19 @@ class CacheClient {
 /// {@endtemplate}
 class AuthRepository {
   /// {@macro authentication_repository}
+  /// {@macro authentication_repository}
   AuthRepository({
     CacheClient? cache,
     firebase_auth.FirebaseAuth? firebaseAuth,
     GoogleSignIn? googleSignIn,
-  })  :  _cache = cache ?? CacheClient(),
+  })  : _cache = cache ?? CacheClient(),
         _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
         _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
 
   final CacheClient _cache;
   final firebase_auth.FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
+
 
   //FirebaseManager firebaseManager = new FirebaseManager();
   //User _user =   firebase_auth.FirebaseAuth.instance.currentUser;
@@ -185,6 +237,7 @@ class AuthRepository {
   /// the authentication state changes.
   ///
   /// Emits [User.empty] if the user is not authenticated.
+
   Stream<User> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
       final user = firebaseUser == null ? User.empty : firebaseUser.toUser;
@@ -216,18 +269,20 @@ class AuthRepository {
       );
 
 
-    } on Exception {
-      debugPrint("Exception : Sign Up With Mail failed => $Exception ");
-      throw SignUpFailure();
     }
-    /*on firebase_auth.FirebaseAuthException catch (e) {
+    on firebase_auth.FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-        print('No user found for that email.');
+        debugPrint('No user found for that email.');
       } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+        debugPrint('Wrong password provided for that user.');
       }
-    }*/
+    }catch (_) {
+      debugPrint("Exception : Sign Up With Mail failed => $Exception ");
+      throw const SignUpWithEmailAndPasswordFailure();
+    }
   }
+
+
 
   /// Starts the Sign In Anonymously .
   ///
@@ -244,7 +299,7 @@ class AuthRepository {
   /// Starts the Sign In with Google Flow.
   ///
   /// Throws a [LogInWithEmailAndPasswordFailure] if an exception occurs.
-  Future<void> logInWithGoogle() async {
+  /*Future<void> logInWithGoogle() async {
 
     try {
       final googleUser = await _googleSignIn.signIn();
@@ -259,24 +314,51 @@ class AuthRepository {
       debugPrint("Exception : Login With Google failed => $Exception ");
       throw LogInWithGoogleFailure();
     }
+  }*/
+
+  Future<void> logInWithGoogle() async {
+    try {
+      late final firebase_auth.AuthCredential credential;
+      if (isWeb) {
+        final googleProvider = firebase_auth.GoogleAuthProvider();
+        final userCredential = await _firebaseAuth.signInWithPopup(
+          googleProvider,
+        );
+        credential = userCredential.credential!;
+      } else {
+        final googleUser = await _googleSignIn.signIn();
+        final googleAuth = await googleUser!.authentication;
+        credential = firebase_auth.GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+      }
+
+      await _firebaseAuth.signInWithCredential(credential);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      throw LogInWithGoogleFailure.fromCode(e.code);
+    } catch (_) {
+      throw const LogInWithGoogleFailure();
+    }
   }
 
   /// Signs in with the provided [email] and [password].
   ///
   /// Throws a [LogInWithEmailAndPasswordFailure] if an exception occurs.
+
   Future<void> logInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
-    //assert(email != null && password != null);
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-    } on Exception {
-      //print();
-      throw LogInWithEmailAndPasswordFailure();
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      throw LogInWithEmailAndPasswordFailure.fromCode(e.code);
+    } catch (_) {
+      throw const LogInWithEmailAndPasswordFailure();
     }
   }
 
@@ -319,6 +401,7 @@ class AuthRepository {
 extension on firebase_auth.User {
   //bool get emailNotVerified => !emailVerified;
 
+  /// Maps a [firebase_auth.User] into a [User].
   User get toUser {
 
     return User(
