0f3295501739fdecaae4da800cc04bfc187af0eb
lordyhas <dev.haspro@gmail.com>
2023-11-01 08:34:51 +0200

    version 0.2.2+1

--- lib/navigation_home_screen.dart
+++ lib/navigation_home_screen.dart
@@ -2,7 +2,7 @@ import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:knote/setting_profile_screen.dart';
+import 'package:knote/src/pages/setting_profile_screen.dart';
 import 'package:knote/src/pages/custom_drawer/home_drawer.dart';
 
 import 'data/database/firebase_manager.dart';
