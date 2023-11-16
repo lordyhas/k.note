e74207d8f18ca747dc6474a1f3e67f1c10ee51a1
lordyhas <dev.haspro@gmail.com>
2023-11-16 21:55:51 +0200

    revised | version 0.2.2+5

--- lib/data/app_bloc.dart
+++ lib/data/app_bloc.dart
@@ -1,5 +1,6 @@
 library data.app_bloc;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 export 'app_bloc/authentication/authentication_bloc.dart';
@@ -11,32 +12,18 @@ export 'theme_and_language_cubit.dart';
 /// Custom [BlocObserver] which observes all bloc and cubit instances.
 ///
 
-
 class AppBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
-    print(bloc.toString() + " => onEvent() : $event");
+    debugPrint("$bloc => onEvent() : $event");
     super.onEvent(bloc, event);
   }
 
-  /*@override
-  void onChange(Cubit cubit, Change change) {
-    print(cubit.toString() + " => onChange() : $change");
-    super.onChange(cubit, change);
-  }*/
-
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print(bloc.toString() + " => onTransition()  : $transition");
+    debugPrint("$bloc => onTransition()  : $transition");
     super.onTransition(bloc, transition);
   }
-
-/* @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    print("onError()  : $error");
-    //print(error);
-    super.onError(bloc, error, stackTrace);
-  }*/
 
 }
 
